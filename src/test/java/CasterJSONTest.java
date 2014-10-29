f6df66d736f84be877569ce5d04347de9745e545
demoth <demoth.cadaver@gmail.com>
2014-10-29 14:33:23 -0700

    fixed test

--- src/test/java/CasterJSONTest.java
+++ src/test/java/CasterJSONTest.java
@@ -1,11 +1,6 @@
 import org.demoth.jcaster.CasterProtocol;
 import org.demoth.jcaster.CasterProtocolJSON;
-import org.demoth.jcaster.Game;
 import org.demoth.jcaster.ServerWriter;
-import org.demoth.jcaster.messages.CreatureMessage;
-import org.demoth.jcaster.messages.EnvironmentMessage;
-import org.demoth.jcaster.messages.EventMessage;
-import org.demoth.jcaster.messages.InfoMessage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,29 +12,8 @@ public class CasterJSONTest {
     @Test
     public void testLogin() {
         StringBuilder buffer = new StringBuilder();
-        Game testGame = new Game() {
-            @Override
-            public void updateEnvironment(EnvironmentMessage jsonMsg) {
-
-            }
-
-            @Override
-            public void updateCreature(CreatureMessage jsonMsg) {
-
-            }
-
-            @Override
-            public void updateInfo(InfoMessage jsonMsg) {
-
-            }
-
-            @Override
-            public void updateEvent(EventMessage jsonMsg) {
-
-            }
-        };
         ServerWriter testServerWriter = buffer::append;
-        CasterProtocol jsonp = new CasterProtocolJSON(testServerWriter, testGame);
+        CasterProtocol jsonp = new CasterProtocolJSON(testServerWriter, null);
         jsonp.login("doktor", "robotnik");
         Assert.assertEquals("{\"what\":\"login\",\"login\":\"doktor\",\"password\":\"robotnik\"}", buffer.toString());
     }
